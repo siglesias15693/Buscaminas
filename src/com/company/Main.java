4b3d2ecc932c5e14d5a1608fd14c5beb3b9e0eb5
Samuel_Iglesias_Iglesias <siglesias15693@alumnes.iesmanacor.cat>
2022-05-05 12:19:32 +0200

    commit 13

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,194 +2,19 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main{
+    public static String[][] tablero;
+    public static int[][] tablerominas;
+    public static int Columnas;
+    public static int Filas;
+    public static int minas;
+    public static int cassillasGiradas;
+    public static int cassillasTotales;
+    public static boolean running = true;
 
-    private static String[][] tablero;
-    private static int[][] tablerominas;
-    private static int Columnas;
-    private static int Filas;
-    private static int minas;
-    private static boolean running = true;
-    private static int cassillasGiradas;
-    private static int cassillasTotales;
-
-    private static boolean verificador=true;
-    private static String mensajeError= "";
-
-    private static final Scanner lector = new Scanner(System.in);
-
-    public static void mostrarTablero() {
-        System.out.print("         ");
-        for (int i = 0; i < Columnas; i++) {
-            System.out.print("___ ");
-        }System.out.println();
-
-        int n=Filas+1;
-        for (int i = 0; i < Filas; i++) {
-            n--;
-            System.out.print("     "+n+"  |");
-            for (int j = 0; j < Columnas; j++) {
-
-                switch (tablero[j][i]){
-                    case " ":System.out.print(tablero[j][i]);break;
-                    case "X":System.out.print("\033[41m " + tablero[j][i] + " \u001B[0m");break;
-                    case "0":System.out.print("\033[47m " + tablero[j][i] + " \u001B[0m");break;
-                    case "1":System.out.print("\033[44m " + tablero[j][i] + " \u001B[0m");break;
-                    case "2":System.out.print("\033[42m " + tablero[j][i] + " \u001B[0m");break;
-
-                    default:System.out.print("\033[35m" + tablero[j][i] + "\u001B[0m");break;
-                }
-
-                System.out.print("|");
-            }
-            System.out.println();
-            if (i < Filas -1) {
-                System.out.print("        |");
-                for (int p = 0; p < Columnas ; p++) {
-                    if (p==Columnas-1){
-                        System.out.print("--–");
-                    }else{
-                        System.out.print("--– ");
-                    }
-                }
-                System.out.println("|");
-            }
-        }
-        System.out.print("         ");
-        for (int i = 1; i < Columnas + 1; i++) {
-            System.out.print("¯¯¯ ");
-        }
-        System.out.println();
-
-        System.out.print("          ");
-        for (int i = 1; i < Columnas + 1; i++) {
-            System.out.print(i + "   ");
-        }System.out.println();
-
-
-        System.out.println("=============================================||| N°Minas " + minas + " |||");
-        System.out.println("==============================  Espacios restantes:  "+(cassillasTotales-cassillasGiradas)+"   =====");
-    }
-
-    public static void jugada( int x,int y) {
-        if (tablero[x][y].equals("   ")){
-            cassillasGiradas++;
-            if (tablerominas[x][y] < 0) {
-                tablero[x][y] = "X";
-                mostrarTablero();
-                System.out.print("\033[31mGAME OVER");
-                running=false;
-            } else {
-                tablero[x][y] = String.valueOf(tablerominas[x][y]);
-            }
-
-            if (tablerominas[x][y] == 0) {
-                esCero(x,y);
-            }
-        }
-
-        comprovarVictoria();
-    }
-
-    public static void comprovarVictoria(){
-        if (cassillasGiradas>=cassillasTotales){
-            mostrarTablero();
-            System.out.println("\033[32m¡¡¡Victoria!!!!!");
-            running=false;
-        }
-    }
-
-    public static void esCero(int x, int y){
-        if (x - 1 >= 0 && y - 1 >= 0) {jugada(x-1,y-1);}
-
-        if (x - 1 >= 0) {jugada(x-1,y);}
-
-        if (x - 1 >= 0 && y + 1 < Filas) {jugada(x-1,y+1);}
-
-        if (y - 1 >= 0 ) {jugada(x,y-1);}
-
-        if (y + 1 < Filas ) {jugada(x,y+1);}
-
-        if (x + 1 < Columnas && y - 1 >= 0 ) {jugada(x+1,y-1);}
-
-        if (x + 1 < Columnas) {jugada(x+1,y);}
-
-        if (x + 1 < Columnas && y + 1 < Filas) {jugada(x+1,y+1);}
-    }
-
-    public static void eleccion(){
-        while (verificador) {
-            System.out.println(mensajeError);
-            mensajeError = "";
-
-            System.out.print("Introduzca el numero de fila: ");
-            int fila = lector.nextInt();
-            fila=Filas-fila;
-
-            System.out.print("Introduzca el numero de columna: ");
-            int columna = lector.nextInt()-1;
-
-
-            if (fila >= 0 && fila < Filas && columna >= 0 && columna < Columnas) {
-                if (tablero[columna][fila].equals("   ")) {
-                    jugada(columna,fila);
-                    verificador = false;
-                }else{
-                    mensajeError = "\n\033[35m**ERROR:\u001B[0m Esta posicion ya esta verificada";
-                }
-            } else {
-                mensajeError = "\n\033[35m**ERROR:\u001B[0m La fila o columna esta fuera de rango";
-            }
-
-        }
-        verificador = true;
-    }
-
-    public static void play(){
-        while (running) {
-            mostrarTablero();
-            eleccion();
-        }
-    }
-
-    public static void assignarMinas(){
-        int m =0;
-        while (m<minas) {
-            int x = (int) (Math.random() * Columnas);
-            int y = (int) (Math.random() * Filas);
-
-            if (tablerominas[x][y]==0) {
-                tablerominas[x][y] = -1;
-                m++;
-            }
-        }
-    }
-
-    public static void assignarValores(){
-        for (int x=0;x<Columnas;x++){
-            for (int y=0;y<Filas;y++){
-                if (tablerominas[x][y]==-1){
-
-                    if (x-1>=0 && y-1>=0 && tablerominas[x-1][y-1]!=-1){tablerominas[x-1][y-1]++;}
-
-                    if (x-1>=0 && tablerominas[x-1][y]!=-1){tablerominas[x-1][y]++;}
-
-                    if (x-1>=0 && y+1<Filas && tablerominas[x-1][y+1]!=-1){tablerominas[x-1][y+1]++;}
-
-                    if (y-1>=0 && tablerominas[x][y-1]!=-1){tablerominas[x][y-1]++;}
-
-                    if (y+1<Filas && tablerominas[x][y+1]!=-1){tablerominas[x][y+1]++;}
-
-                    if (x+1<Columnas && y-1>=0 && tablerominas[x+1][y-1]!=-1){tablerominas[x+1][y-1]++;}
-
-                    if (x+1<Columnas && tablerominas[x+1][y]!=-1){tablerominas[x+1][y]++;}
-
-                    if (x+1<Columnas && y+1<Filas && tablerominas[x+1][y+1]!=-1){tablerominas[x+1][y+1]++;}
-
-                }
-            }
-        }
-    }
+    public static boolean verificador=true;
+    public static String mensajeError= "";
+    public static final Scanner lector = new Scanner(System.in);
 
     public static void main(String[] args) {
         tablero = new String[9][9];
@@ -208,11 +33,65 @@ public class Main {
             }
         }
 
-        //Se asigna las minas y valores al tablero
         assignarMinas();
         assignarValores();
+        play.play();
+    }
 
-        play();
+    public static void assignarMinas() {
+        int m = 0;
+        while (m < minas) {
+            int x = (int) (Math.random() * Columnas);
+            int y = (int) (Math.random() * Filas);
+
+            if (tablerominas[x][y] == 0) {
+                tablerominas[x][y] = -1;
+                m++;
+            }
+        }
+    }
+
+    public static void assignarValores() {
+        for (int x = 0; x < Columnas; x++) {
+            for (int y = 0; y < Filas; y++) {
+                if (tablerominas[x][y] == -1) {
+
+                    if (x - 1 >= 0 && y - 1 >= 0 && tablerominas[x - 1][y - 1] != -1) {
+                        tablerominas[x - 1][y - 1]++;
+                    }
+
+                    if (x - 1 >= 0 && tablerominas[x - 1][y] != -1) {
+                        tablerominas[x - 1][y]++;
+                    }
+
+                    if (x - 1 >= 0 && y + 1 < Filas && tablerominas[x - 1][y + 1] != -1) {
+                        tablerominas[x - 1][y + 1]++;
+                    }
+
+                    if (y - 1 >= 0 && tablerominas[x][y - 1] != -1) {
+                        tablerominas[x][y - 1]++;
+                    }
+
+                    if (y + 1 < Filas && tablerominas[x][y + 1] != -1) {
+                        tablerominas[x][y + 1]++;
+                    }
+
+                    if (x + 1 < Columnas && y - 1 >= 0 && tablerominas[x + 1][y - 1] != -1) {
+                        tablerominas[x + 1][y - 1]++;
+                    }
+
+                    if (x + 1 < Columnas && tablerominas[x + 1][y] != -1) {
+                        tablerominas[x + 1][y]++;
+                    }
+
+                    if (x + 1 < Columnas && y + 1 < Filas && tablerominas[x + 1][y + 1] != -1) {
+                        tablerominas[x + 1][y + 1]++;
+                    }
+
+                }
+            }
+        }
     }
 
 }
+
